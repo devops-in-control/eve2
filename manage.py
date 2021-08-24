8396c9e26491602980084cd4612ea216b6e6c503
LJ on AWS <devops-in-control@hotmail.com>
2021-08-24 11:15:46 +0000

    Fix failed merge resovle

--- manage.py
+++ manage.py
@@ -7,11 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 18f7193d4c7b364ab909b8e217bd30b844fd6bff
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
